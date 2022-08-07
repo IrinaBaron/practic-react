b9f258fa810ea9be645dd6c494e26d4f75c85b59
Irina Baron <irr.baron@gmail.com>
2022-08-07 12:36:24 +0300

    added files, start project

---
+++ my-app/src/App.js
@@ -0,0 +1,39 @@
+// import logo from './logo.svg';
+import './App.css';
+
+import React from 'react';
+
+function App() {
+  return (
+    <div className="wrapper">
+      <p className='blue-text'>Lorem ipsum</p> 
+      <p className='green-text'>some text</p>
+      <input type="text" />
+      <br />
+      <input type="number" />
+
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+        <li>6</li>
+        <li>7</li>
+        <li>8</li>
+        <li>9</li>
+        <li>10</li>
+      </ul>
+
+      <table>
+        <thead>
+          <tr>
+            <td>table</td>
+          </tr>
+        </thead>
+      </table>
+    </div>
+  );
+}
+
+export default App;
