07d3cf53a423fc8433611fbf295cf869231fefad
Irina Baron <irr.baron@gmail.com>
2022-08-13 15:46:14 +0300

    change App

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,36 +4,22 @@ import './App.css';
 import React from 'react';
 
 function App() {
-  // const arr = [ 1, 2, 3, 4, 5];
-  // const obj = {name: 'John', surname: 'Smith'};
-  // const li1 = <li>text1</li>;
-  // const li2 = <li>text2</li>;
-  // const li3 = <li>text3</li>;
-  const attr = 'block';
-  return (
-    <div className="wrapper">
-      <div id={attr}>
-        text
-      </div>
-      {/* <ul>
-        {li1}
-        {li2}
-        {li3}
-      </ul> */}
-      {/* <p>
-        name: <span>{obj.name}</span>, 
-        <br />
-        surname: <span>{obj.surname}</span>
-      </p> */}
-      {/* <ul>
-        <li>{arr[0]}</li>
-        <li>{arr[1]}</li>
-        <li>{arr[2]}</li>
-        <li>{arr[3]}</li>
-        <li>{arr[4]}</li>
-      </ul> */}
-    </div>
-  );
+  // const age = 19;
+  // const isAuth = false;
+  function square(num) {
+    return num ** 2;
+  }
+
+  function cube(num) {
+    return num ** 3;
+  }
+
+  const sum = square(3) + cube(4);
+  return <div>
+    { sum}
+    {/* { age > 18 ? <p>more 18</p> : <p>small 18</p> } */}
+    {/* { !isAuth && <p>вы авторизованы</p> } */}
+  </div>
 }
 
 export default App;
