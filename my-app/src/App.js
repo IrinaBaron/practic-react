5256921f34adcffcb205490511c1df0ac1429525
Irina Baron <irr.baron@gmail.com>
2022-09-25 21:19:22 +0300

    attr key

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,23 +3,23 @@ import './App.css';
 
 import React from 'react';
 
+const users = [
+  { id: 123, name: 'user1', surn: 'surname1', age: 30},
+  { id: 456, name: 'user2', surn: 'surname2', age: 31},
+  { id: 789, name: 'user3', surn: 'surname3', age: 32},
+]
+
 function App() {
-  // const age = 19;
-  // const isAuth = false;
-  function square(num) {
-    return num ** 2;
-  }
-
-  function cube(num) {
-    return num ** 3;
-  }
-
-  const sum = square(3) + cube(4);
-  return <div>
-    { sum}
-    {/* { age > 18 ? <p>more 18</p> : <p>small 18</p> } */}
-    {/* { !isAuth && <p>вы авторизованы</p> } */}
-  </div>
+  const res = users.map(function(item) {
+    return <li key={item.id}>
+      Name: {item.name};
+      Surname: {item.surn};
+      Age: {item.age}
+    </li>
+  })
+  return <ul>
+    { res }
+  </ul>
 }
 
 export default App;
