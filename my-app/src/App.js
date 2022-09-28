c3947d115644166eb8cebc2532b6df92d3369c94
Irina Baron <irr.baron@gmail.com>
2022-09-28 19:00:44 +0300

    added plugin

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,11 +2,12 @@
 import './App.css';
 
 import React from 'react';
+import uuid from 'react-uuid';
 
 const users = [
-  { id: 123, name: 'user1', surn: 'surname1', age: 30},
-  { id: 456, name: 'user2', surn: 'surname2', age: 31},
-  { id: 789, name: 'user3', surn: 'surname3', age: 32},
+  { id: uuid(), name: 'user1', surn: 'surname1', age: 30},
+  { id: uuid(), name: 'user2', surn: 'surname2', age: 31},
+  { id: uuid(), name: 'user3', surn: 'surname3', age: 32},
 ]
 
 function App() {
