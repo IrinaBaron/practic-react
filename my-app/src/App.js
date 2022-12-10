a559c9803267c5d2abd0fadca38186de9d0d4d4c
Irina Baron <irr.baron@gmail.com>
2022-12-10 13:43:38 +0200

    change data

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,56 @@
 // import logo from './logo.svg';
 import './App.css';
 
-import React from 'react';
-import uuid from 'react-uuid';
+import React, { useState } from 'react';
+// import uuid from 'react-uuid';
 
-const users = [
-  { id: uuid(), name: 'user1', surn: 'surname1', age: 30},
-  { id: uuid(), name: 'user2', surn: 'surname2', age: 31},
-  { id: uuid(), name: 'user3', surn: 'surname3', age: 32},
-]
+// const users = [
+//   { id: uuid(), name: 'user1', surn: 'surname1', age: 30},
+//   { id: uuid(), name: 'user2', surn: 'surname2', age: 31},
+//   { id: uuid(), name: 'user3', surn: 'surname3', age: 32},
+// ]
+
+// function App() {
+//   const res = users.map(function(item) {
+//     return <li key={item.id}>
+//       Name: {item.name};
+//       Surname: {item.surn};
+//       Age: {item.age}
+//     </li>
+//   })
+//   return <ul>
+//     { res }
+//   </ul>
+// }
+
+// function App() {
+//   let [inCart, setInCart] = useState(false);
+
+//   return <div>
+//     <span>{inCart ? 'в корзине' : 'не в корзине'}</span>
+
+//     <button onClick={() => setInCart(!inCart)}>btn</button>
+//   </div>
+// }
 
 function App() {
-  const res = users.map(function(item) {
-    return <li key={item.id}>
-      Name: {item.name};
-      Surname: {item.surn};
-      Age: {item.age}
-    </li>
-  })
-  return <ul>
-    { res }
-  </ul>
+  let [name, setName] = useState('Ivan');
+  let [surname, setSurname] = useState('Ivanov');
+  let [age, setAge] = useState('20');
+  let [inStatus, setStatus] = useState(false);
+
+  return <div>
+    <span>{name}</span>
+    <span>{surname}</span>
+    <span>{age}</span>
+    <span>{inStatus ? 'заблокирован' : 'не заблокирован'}</span>
+    <button onClick={() => setName('Dimitar')}>change name</button>
+    <button onClick={() => setSurname('Dimitrov')}>change surname</button>
+    {/* <button onClick={() => setStatus('заблокирован')}>заблокировать</button> */}
+
+    {/* <button onClick={() => setStatus('не заблокирован')}>разблокировать</button> */}
+    <button onClick={() => setStatus(!inStatus)}>{inStatus ? 'разблокировать' : 'заблокировать'}</button>
+  </div>
 }
 
 export default App;
